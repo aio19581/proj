854f96cbf6bdeca0ccb996ecd7a652aac8ec4e7d
ksh <aiostpch@naver.com>
2024-12-26 15:07:40 +0900

    create

---
+++ src/main/java/com/example/gethome/GethomeApplication.java
@@ -0,0 +1,13 @@
+package com.example.gethome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GethomeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GethomeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gethome/GethomeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gethome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GethomeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
